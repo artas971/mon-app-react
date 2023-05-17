b3ad76ab151a573806341e0ae330f2202bfd47b1
steve <artas971@gmail.com>
2023-05-17 11:29:38 +0200

    react

---
+++ src/components/red.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from 'styled-compenents'
+
+const Red =() =>{
+    <React.Fragment>
+    return <mainContainer>Hello world again !</mainContainer>;
+    <Red />
+    </React.Fragment>
+};
+const mainContainer = styled.h2 `
+`
+export default red ;
